11057329e19235660c4fd810c9b006bfdc2cad38
EllieD33 <120728802+EllieD33@users.noreply.github.com>
2024-09-13 16:03:49 +0100

    updates property in relationshiptype for clarity

--- Models/RelationshipType.cs
+++ Models/RelationshipType.cs
@@ -5,7 +5,7 @@ namespace CelebrationReminderAPI.Models
     public class RelationshipType
     {
         public int RelationshipTypeId { get; set; }
-        public string RelationshipType { get; set; }
+        public string RelationshipName { get; set; }
 
         public ICollection<Contact> Contacts { get; set; } = new List<Contact>();
     }
