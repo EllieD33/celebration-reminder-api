aa09bf709ff10efdb9686c573c6ee763815b13b2
EllieD33 <120728802+EllieD33@users.noreply.github.com>
2024-09-13 15:36:40 +0100

    updates address model

--- Models/Address.cs
+++ Models/Address.cs
@@ -1,4 +1,5 @@
 using System.ComponentModel.DataAnnotations;
+using System.Collections.Generic;
 
 namespace CelebrationReminderAPI.Models
 {
@@ -17,5 +18,6 @@ namespace CelebrationReminderAPI.Models
         public int CountryId { get; set; }
 
         public Country Country { get; set; }
+        public ICollection<Contact> Contacts { get; set; } = new List<Contact>();
     }
 }
