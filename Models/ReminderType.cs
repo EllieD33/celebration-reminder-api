e53f2870142629d42eac2bd2626a5956ad133cc2
EllieD33 <120728802+EllieD33@users.noreply.github.com>
2024-09-25 12:40:14 +0100

    adds string length annotation to ReminderType  model

--- Models/ReminderType.cs
+++ Models/ReminderType.cs
@@ -1,10 +1,12 @@
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace CelebrationReminderAPI.Models
 {
     public class ReminderType
     {
         public int ReminderTypeId { get; set; }
+        [StringLength(50)]
         public string ReminderName { get; set; }
 
         public ICollection<Reminder> Reminders { get; set; } = new List<Reminder>();
