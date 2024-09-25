4e9cc19e409194b27b1a19d1dc958ee59c5143d9
EllieD33 <120728802+EllieD33@users.noreply.github.com>
2024-09-25 12:32:17 +0100

    adds string length annotation to CommemorationType model

--- Models/CommemorationType.cs
+++ Models/CommemorationType.cs
@@ -1,10 +1,12 @@
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace CelebrationReminderAPI.Models
 {
     public class CommemorationType
     {
         public int CommemorationTypeId { get; set; }
+        [StringLength(50)]
         public string CommemorationName { get; set; }
 
         public ICollection<Event> Events { get; set; } = new List<Event>();
