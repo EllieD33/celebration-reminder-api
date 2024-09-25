b252ca0d9f9bebd93e994160d88fdf7b61a0c80a
EllieD33 <120728802+EllieD33@users.noreply.github.com>
2024-09-25 12:35:54 +0100

    adds string length annotation to Country model

--- Models/Country.cs
+++ Models/Country.cs
@@ -1,10 +1,12 @@
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace CelebrationReminderAPI.Models
 {
     public class Country
     {
         public int CountryId {get; set; }
+        [StringLength(100)]
         public string CountryName { get; set; }
 
         public ICollection<User> Users { get; set; } = new List<User>();
