c616f9b79f7c156a8d2c43a8752d3accc270100a
EllieD33 <120728802+EllieD33@users.noreply.github.com>
2024-09-25 12:38:42 +0100

    adds string length annotation to RelationshipType  model

--- Models/RelationshipType.cs
+++ Models/RelationshipType.cs
@@ -1,10 +1,12 @@
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace CelebrationReminderAPI.Models
 {
     public class RelationshipType
     {
         public int RelationshipTypeId { get; set; }
+        [StringLength(50)]
         public string RelationshipName { get; set; }
 
         public ICollection<Contact> Contacts { get; set; } = new List<Contact>();
