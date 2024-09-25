a61a1f2b826c1d1932e319e209dc7bd954733f2d
EllieD33 <120728802+EllieD33@users.noreply.github.com>
2024-09-25 12:42:21 +0100

    adds string length annotation to Role model

--- Models/Role.cs
+++ Models/Role.cs
@@ -1,10 +1,12 @@
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace CelebrationReminderAPI.Models
 {
     public class Role
     {
         public int RoleId { get; set; }
+        [StringLength(50)]
         public string RoleName { get; set; }
 
         public ICollection<User> Users { get; set; } = new List<User>();
